9755bfd6687dd267ef3b01e11e2ef1565235dba2
Kateryna083 <katyasmile083@gmail.com>
2024-08-17 16:02:25 +0300

    hw-2

---
+++ src/components/App/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notifications/Notification";
+
+import "./App.css";
+
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("feedback");
+    if (savedFeedback) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
+  const handleReset = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+
+      <Options
+        totalFeedback={totalFeedback}
+        updateFeedback={updateFeedback}
+        handleReset={handleReset}
+      />
+
+      {totalFeedback > 0 && (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}
+      
+      {totalFeedback == 0 && <Notification />}
+    </>
+  );
+}
